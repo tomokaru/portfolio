cdf1f34acc4edf4544def1aa54dffd4e161c04ca
tomotomo0511 <tomokaru0511@gmail.com>
2022-12-04 14:08:57 +0900

    [update]

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ $('.js_hamburger').on('click', function () {
   $('.js_header-nav').toggleClass('on');
   $('.js_hamburger-btn_line').toggleClass('on');
 })
+$('.js_header-nav a[href]').on('click', function(event) {
+  $('.js_hamburger').trigger('click');
+});
 
 //ページ遷移時のローディング
 $(window).on('load',function(){
@@ -20,3 +23,15 @@ setInterval(() => {
     $target.removeClass(classname);
   }, timeout);
 }, timeout * 0.00001);
+
+// スムーススクロール
+const header = $('.ly_header');
+$('.smooth a').click(function () {
+    var gap = header.outerHeight();
+    var speed = 600;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - gap;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+});
